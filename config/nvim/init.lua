dbaf7d5a39fbb69b6a7bcb26e2e5686c3fe9d572
Andrew Vogel <andrewpvogel@gmail.com>
2023-05-19 17:49:06 -0400

    feat: add turbux prepend command for ruby tests

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -382,3 +382,5 @@ vim.o.splitright = true
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.g.turbux_command_prefix = 'bundle exec' -- default: (empty)
