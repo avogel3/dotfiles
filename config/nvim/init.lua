196ffa59a0b6bb0cd51abc5de81137b8db216cbd
Andrew Vogel <andrewpvogel@gmail.com>
2023-12-12 15:49:02 -0500

    chore(nvim): update lsp snippet plugin with updates from kickstart

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -74,11 +74,18 @@ require('lazy').setup({
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  },
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
 
-  { -- Formatting
-    'lukas-reineke/lsp-format.nvim',
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
   },
 
   {
