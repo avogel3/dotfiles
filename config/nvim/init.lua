4ffc7d111c012f4b12167c2ae55c985ca51e6b2c
Andrew Vogel <andrewpvogel@gmail.com>
2023-12-12 14:47:55 -0500

    feat(nvim): remove whichkey plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -81,8 +81,6 @@ require('lazy').setup({
     'lukas-reineke/lsp-format.nvim',
   },
 
-  -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',          opts = {} },
   {
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
