340a9f611cde513ae965ef9658ea30624127837a
Andrew Vogel <andrewpvogel@gmail.com>
2025-11-25 17:40:03 -0700

    fix(nvim): temporarily disable problematic validation of projectionist config file
    this broke starting in nvim v0.11.5

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -389,26 +389,28 @@ vim.opt.ttimeoutlen = 100
 
 vim.g.turbux_command_prefix = 'bundle exec' -- default: (empty)
 
-local function run_script_with_cwd(command, cwd, error_message)
-  -- Run the command and capture the exit code
-  local exit_code = os.execute(command)
-
-  -- Check the exit status
-  if exit_code ~= 0 then
-    vim.schedule(function()
-      vim.notify(
-       error_message .. " " .. exit_code,
-        vim.log.levels.ERROR
-      )
-    end)
-  end
-end
-
--- Check if a projections file is defined and valid JSON
-local config_dir = vim.fn.stdpath("config")
-local projections_check_script = config_dir .. "/projections_check.rb"
-run_script_with_cwd(
-  string.format("ruby %s %s", projections_check_script, vim.fn.getcwd()),
-  vim.fn.getcwd,
-  "Custom .projections file is invalid"
-)
+-- FIXME: breaks config since v0.11.5
+--
+-- local function run_script_with_cwd(command, cwd, error_message)
+--   -- Run the command and capture the exit code
+--   local exit_code = os.execute(command)
+--
+--   -- Check the exit status
+--   if exit_code ~= 0 then
+--     vim.schedule(function()
+--       vim.notify(
+--        error_message .. " " .. exit_code,
+--         vim.log.levels.ERROR
+--       )
+--     end)
+--   end
+-- end
+--
+-- -- Check if a projections file is defined and valid JSON
+-- local config_dir = vim.fn.stdpath("config")
+-- local projections_check_script = config_dir .. "/projections_check.rb"
+-- run_script_with_cwd(
+--   string.format("ruby %s %s", projections_check_script, vim.fn.getcwd()),
+--   vim.fn.getcwd,
+--   "Custom .projections file is invalid"
+-- )
